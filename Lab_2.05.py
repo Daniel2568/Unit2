20d32284c647c621c18e11d4e9d8b078412bb96f
Daniel2568 <dworth25@students.bmchs.org>
2022-02-09 21:17:41 -0600

    second edit

--- Lab_2.05.py
+++ Lab_2.05.py
@@ -10,16 +10,16 @@ a = ['a', 'b', 'c', 'd', 'e']
 print(a[0:3])
 print(a[1:4])
 
-Prediction:
-Actual:
+Prediction: print(a[0:3]) will print ['a','b','c'] and print(a[1:4]) will print ['b','c','d','e']
+Actual: print(a[0:3]) = ['a','b','c'] and print(a[1:4]) = ['b', 'c', 'd']
 
 Example 2
 ---------
 a = ['a', 'b', 'c', 'd', 'e']
 print(a[1:len(a) - 3])
 
-Prediction:
-Actual:
+Prediction: I'm not 100% sure because Idk what 'len' means
+Actual: print(a[1:len(a) - 3]) = ['b']
 
 Example 3
 ---------
@@ -28,8 +28,8 @@ b = a.remove('b')
 print(a)
 print(b)
 
-Prediction:
-Actual:
+Prediction: print(a) will be just normal ['a', 'b', 'c', 'd', 'e'] and print(b) will equal ['a', 'c', 'd', 'e']
+Actual: print(a) = ['a', 'c', 'd', 'e']  print(b) = None
 
 Example 4
 ---------
@@ -39,8 +39,8 @@ b = a.pop()
 print(a)
 print(b)
 
-Prediction:
-Actual:
+Prediction: print(a) = ['haha','b','c','d','e'] print(b) = I'm not sure what pop means
+Actual: print(a) = ['haha','b','c','d'] print(b) = e
 
 Example 5
 ---------
@@ -49,8 +49,8 @@ b = a + ['abc']
 print(a)
 print(b)
 
-Prediction:
-Actual:
+Prediction: print(a) will print ('a','abc','b','c','d','e') and print(b) = the same thing??
+Actual: print(a) = ['a', 'b', 'c', 'd', 'e'] print(b) = ['a', 'b', 'c', 'd', 'e', 'abc']
 
 Example 6
 ---------
@@ -59,8 +59,8 @@ b = a.append('f')
 print(a)
 print(b)
 
-Prediction:
-Actual:
+Prediction: print(a) will probably print ['a', 'b', 'c', 'd', 'e'] and im not sure what print(b) will do because Idk what append does
+Actual: print(a) = ['a', 'b', 'c', 'd', 'e', 'f'] print(b) = None
 2. In script mode (Type your program below the multi-line comment)
 We are going to start implementing Tic-Tac-Toe using a single list.
 1. The user picks a location on the board according to the number:
@@ -103,4 +103,28 @@ turn = int(input("Pick a number and that's where your   x   will go."))
 
 
 if turn == 1:
-    
+    board[0][0] = 'x'
+elif turn == 2:
+    board[0][1] = 'x'
+elif turn == 3: 
+    board[0][2] = 'x'
+elif turn == 4:
+    board[1][0] = 'x'
+elif turn == 5:
+    board[1][1] = 'x'
+elif turn == 6:
+    board[1][2] = 'x'
+elif turn == 7:
+    board[2][0] = 'x'
+elif turn == 8:
+    board[2][1] = 'x'
+elif turn == 9:
+    board[2][2] = 'x'
+
+#update board
+
+print(f"{board[0][0]} | {board[0][1]} | {board[0][2]}")
+print("---------")
+print(f"{board[1][0]} | {board[1][1]} | {board[1][2]}")
+print("---------")
+print(f"{board[2][0]} | {board[2][1]} | {board[2][2]}")
