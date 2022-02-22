71088ac08a18609ddee5d0b362d5e06486d1b066
Daniel Worth <dworth25@students.bmchs.org>
2022-02-22 09:44:21 -0600

    One more thing left

--- text_monster_solution.py
+++ text_monster_solution.py
@@ -1,5 +1,20 @@
 # Text Monster Game 
 
+print("Welcome to Text Monster Adventure!")
+print("You live in a small town in the mythical world of magic. One day you wake up and check your mail")
+print("Player: Oh, whats this? A letter from the Grand Wizard Gandolf.")
+print("You open the letter. The letter says:\nDear Player,\nI am in need of some help that may require some explanation.\nCome as soon as you can.\nfrom, Gandolf-")
+print("You get ready and go to his house")
+print("You knock on the door")
+print("Gandolf answers: Ahh I see you have found my letter. Come in I have things to tell you\nyou walk into the house and go into the dining room")
+print("Gandolf: I have a problem, You see I have some stone that have gotten stolen from me.\nNow I know who stole them but the problem is getting them back\nI am getting very old and need you to get them for me.\nIt is a lot to ask but if you're up for the task you will be rewarded handsomely")
+print("You think about the quest you are given.\nGandolf: Oh and before you make a final decision the quest will take place in the dungeon of Garabond,\nthere you will find the stones somewhere in the dungeon, there will also be monsters you need to fight\nAll the equipment you will need, you should be able to find in the dungeon\nGoodluck")
+print("You take this into consideration and say: Alright Gandolf, where do I go")
+print("Gandolf gives you a map and tells you where to go")
+print("You make it to the dungeon and find the entrence")
+print("You open the door and find that it is very dark at first, your eyes start to adjust and you start to see everything")
+print("----------------------------------------------")
+
 # Map of dungeon 
 
 
@@ -19,6 +34,7 @@ current_floor = 2
 location = dungeon[current_floor][current_room]
 
 #game loop
+
 while Game_Over == False:
 
     # update location 
@@ -26,20 +42,7 @@ while Game_Over == False:
 
 
     # describe where we are to the user 
-    print("Welcome to Text Monster Adventure!")
-    print("You live in a small town in the mythical world of magic. One day you wake up and check your mail")
-    print("Player: Oh, whats this? A letter from the Grand Wizard Gandolf.")
-    print("You open the letter. The letter says:\nDear Player,\nI am in need of some help that may require some explanation.\nCome as soon as you can.\nfrom, Gandolf-")
-    print("You get ready and go to his house")
-    print("You knock on the door")
-    print("Gandolf answers: Ahh I see you have found my letter. Come in I have things to tell you\nyou walk into the house and go into the dining room")
-    print("Gandolf: I have a problem, You see I have some stone that have gotten stolen from me.\nNow I know who stole them but the problem is getting them back\nI am getting very old and need you to get them for me.\nIt is a lot to ask but if you're up for the task you will be rewarded handsomely")
-    print("You think about the quest you are given.\nGandolf: Oh and before you make a final decision the quest will take place in the dungeon of Garabond,\nthere you will find the stones somewhere in the dungeon, there will also be monsters you need to fight\nAll the equipment you will need, you should be able to find in the dungeon\nGoodluck")
-    print("You take this into consideration and say: Alright Gandolf, where do I go")
-    print("Gandolf gives you a map and tells you where to go")
-    print("You make it to the dungeon and find the entrence")
-    print("You open the door and find that it is very dark at first, your eyes start to adjust and you start to see everything")
-    print("----------------------------------------------")
+
 
     if location == 'empty' :
         print("You are in an empty room. The walls seem to be a bit mossy and gross with... who knows")  
@@ -48,7 +51,7 @@ while Game_Over == False:
     elif location == 'stairs up' :
         print("You see some stairs that seem to be going up") 
     elif location == 'monster' :
-        print("You see a weird thing that has goo all over it and seems to be picking it's nose \n You better get out of there quick before he hits gold and sees you") 
+        print("You see a weird thing that has goo all over it and seems to be picking it's nose\nYou better get out of there quick before he hits gold and sees you") 
     elif location == 'stairs down' : 
         print("You see some stairs going down") 
     elif location == 'magic stones' :
@@ -56,7 +59,7 @@ while Game_Over == False:
     elif location == 'boss monster' :
         print("You found an abnormally big and disformed thing that is very muscular and 10ft tall. It doesn't look very happy, good luck") 
     else: 
-        print("You have found some gold, potions, and a sheild with a sword enscribed with an enchanment saying the sword never breaks")
+        print("You have found some gold, potions, and a sheild with a sword enscribed with an enchanment saying the sword never breaks\nYou might not be able to grab these but you will automatically grab them once the game is over")
     
     #player choices 
     player_choices = input("What would you like to do?[move left, right, up, down, grab, fight, inventory]")
@@ -107,9 +110,8 @@ while Game_Over == False:
                 print(f"the current location is {dungeon[current_floor][current_room]}")
                  
             else: 
-                print(f"That wasn't very smart now wasn't you dummy \n You Died and the moster still remains") #make this more intresting
-                dead = True 
-                break 
+                print(f"That wasn't very smart now wasn't you dummy\nYou Died and the moster still remains") #make this more intresting
+                dead = True  
         if location == 'boss monster' :
             if 'sword' and 'magic stones' in inventory:
                 print("You will try to kill the boss monster") # make this more intresting 
@@ -119,23 +121,31 @@ while Game_Over == False:
                 
                 Game_Over = True 
             else: 
-                print("That wasn't very smart now wasn't you dummy. You Died and the moster still remains") #make this more intresting
+                print("That wasn't very smart now wasn't you dummy.\nYou Died and the moster still remains, if you haven't already go grab the magic stones to defeat the boss monster") #make this more intresting
                 dead = True 
+                Game_Over = True
 
 # determine win/loss
-while Game_Over == True:
-    if dead == True: 
-        print("Game Over")
-        print("You died trying to fight a monster with your fingers")
-    else: 
-         print("You won!")
-         print("You helped the wizard with his problems. He is very thankful and has given you some neat potions and gifts.")
+    if Game_Over == True:
+        if dead == True: 
+            print("Game Over")
+            print("You died trying to fight a monster with your fingers")
+        else: 
+                print("You won!")
+                print("You helped the wizard with his problems. He is very thankful and has given you some neat potions and gifts.")
 
-    # play again?
-    user_input = input("Would you like to play again? (y/n)")
-    if user_input == 'y':
-        dead = False 
-        Game_Over = False
+        # play again?
+        user_input = input("Would you like to play again? (y/n)")
+        if user_input == 'y':
+            # Player info 
+            Game_Over = False 
+            dead = False 
+            inventory = []
+            current_room = 0
+            current_floor = 2
+            location = dungeon[current_floor][current_room]
+        else:
+            print("Thanks for playing, Have a great day")
 
 
 
